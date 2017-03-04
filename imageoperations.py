9dc88779b5cb2b587c9aaf96e415e1b8158a7adc
Teddy Juntunen <tjuntunen@Teds-MacBook-Pro.local>
2017-03-03 17:41:51 -0800

    More functionality

--- imageoperations.py
+++ imageoperations.py
@@ -7,18 +7,24 @@ def main():
     imageWidth, imageHeight = inputImage.size
 
 	# Menu
+    print("-----------------------------")
     print("Choose one of the following: ")
     print("1. Copy")
-    print("2. Flip Vertical")
+    print("2. Flip vertical")
     print("3. Flip horizontal")
     print("4. Lighten")
     print("5. Darken")
-    userChoice = int(input("What do you want to do? (1-5): "))
+    print("6. Scroll horizontal")
+    print("7. Scroll vertical")
+    print("8. Grey scale")
+    print("-----------------------------")
+
+    userChoice = int(input("What do you want to do? (1-8): "))
 
     # Input validation
-    while userChoice > 5 or userChoice < 1:
-    	print("You must choose a number between 1 and 5")
-    	userChoice = int(input("What do you want to do? (1-5): "))
+    while userChoice > 8 or userChoice < 1:
+    	print("You must choose a number between 1 and 8")
+    	userChoice = int(input("What do you want to do? (1-8): "))
 
     if userChoice == 1:
     	copyImage(inputImage, imageWidth, imageHeight)
@@ -32,6 +38,15 @@ def main():
     if userChoice == 5:
         amount = float(input("Enter a float point value between 0 and 1: "))
         darken(amount, inputImage, imageWidth, imageHeight)
+    if userChoice == 6:
+    	numpixels = int(input("Number of pixels to scroll horizontally: "))
+    	scrollHorizontal(numpixels, inputImage, imageWidth, imageHeight)
+    if userChoice == 7:
+    	numpixels = int(input("Number of pixels to scroll vertically: "))
+    	scrollVertical(numpixels, inputImage, imageWidth, imageHeight)
+    if userChoice == 8:
+    	makeGreyscale(inputImage, imageWidth, imageHeight)
+
 
 # Creates a copy of an image given the image variable, its width, and height
 def copyImage(inputImage, imageWidth, imageHeight):
@@ -94,5 +109,57 @@ def darken(amount, inputImage, imageWidth, imageHeight):
             darkenOutput.putpixel((i, j), darkenedPixelColors)
     darkenOutput.save("/Users/tjuntunen/desktop/project2/darker.png")
     print("Image darkened")
+
+def scrollHorizontal(numpixels, inputImage, imageWidth, imageHeight):
+	scrollHorizontalOutput = Image.new('RGB', (imageWidth, imageHeight), 'white')
+
+	while numpixels > imageWidth:
+		numpixels = numpixels // imageWidth
+
+	for i in range(numpixels, imageWidth):
+		for j in range(imageHeight):
+			pixelColors = inputImage.getpixel((i, j))
+			scrollHorizontalOutput.putpixel((i - numpixels, j), pixelColors)
+	for i in range(numpixels):
+		for j in range(imageHeight):
+			pixelColors = inputImage.getpixel((i, j))
+			scrollHorizontalOutput.putpixel((imageWidth - numpixels + i, j), pixelColors)
+	scrollHorizontalOutput.save("/Users/tjuntunen/desktop/project2/scrollhorizontal.png")
+	print("Image scrolled horizontally")
+
+def scrollVertical(numpixels, inputImage, imageWidth, imageHeight):
+	scrollVerticalOutput = Image.new('RGB', (imageWidth, imageHeight), 'white')
+
+	while numpixels > imageHeight:
+		numpixels = numpixels // imageHeight
+
+	for i in range(imageWidth):
+		for j in range(numpixels, imageHeight):
+			pixelColors = inputImage.getpixel((i, j))
+			scrollVerticalOutput.putpixel((i, j - numpixels), pixelColors)
+	for i in range(imageWidth):
+		for j in range(numpixels):
+			pixelColors = inputImage.getpixel((i, j))
+			scrollVerticalOutput.putpixel((i, imageHeight - numpixels + j), pixelColors)
+	scrollVerticalOutput.save("/Users/tjuntunen/desktop/project2/scrollvertical.png")
+	print("Image scrolled vertically")
+
+def makeGreyscale(inputImage, imageWidth, imageHeight):
+	makeGreyscaleOutput = Image.new('RGB', (imageWidth, imageHeight), 'white')
+
+	for i in range(imageWidth):
+		for j in range(imageHeight):
+			pixelColors = inputImage.getpixel((i, j))
+			oldRed = pixelColors[0]
+			oldGreen = pixelColors[1]
+			oldBlue = pixelColors[2]
+			newRed = int(oldRed * 0.30)
+			newGreen = int(oldGreen * 0.59)
+			newBlue = int(oldBlue * 0.11)
+			grey = newRed + newGreen + newBlue
+			greyPixelColors = (grey, grey, grey)
+			makeGreyscaleOutput.putpixel((i, j), greyPixelColors)
+	makeGreyscaleOutput.save("/Users/tjuntunen/desktop/project2/greyscale.png")
+	print("Image greyscaled")
 
 main()
